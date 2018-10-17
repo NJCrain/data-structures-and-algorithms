a677775a5acbf3f7dcb40a9a280b6988efb626bf
Nicholas Crain <nicholascrain@gmail.com>
2018-10-16 20:54:21 -0700

    completed challenge 3

--- code-challenges/301/value-vs-reference/challenges-02.test.js
+++ code-challenges/301/value-vs-reference/challenges-02.test.js
@@ -36,8 +36,8 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 ------------------------------------------------------------------------------------------------ */
 
 const addBirthYearProperty = (obj, year) => {
-  // Solution code here...
-}
+  obj.yearBorn = year;
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -96,7 +96,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should add a property to an object', () => {
     const a = { fullName: 'Octavia Butler' };
     addBirthYearProperty(a, 1947);
