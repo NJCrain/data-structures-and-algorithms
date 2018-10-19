d591b287f92f6bae783dd6b43818900e796b3022
Nicholas Crain <nicholascrain@gmail.com>
2018-10-18 20:53:42 -0700

    completed challenge 2

--- code-challenges/301/regex/challenges-04.test.js
+++ code-challenges/301/regex/challenges-04.test.js
@@ -22,7 +22,8 @@ Return an array containing all the matches.
 ------------------------------------------------------------------------------------------------ */
 
 const isCapitalized = (str) => {
-  // Solution code here...
+  let regex = /[A-Z](\w+)/g;
+  return str.match(regex);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -103,7 +104,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should only return words that begin with a capital letter', () => {
     const capitalResult = isCapitalized('We only want to Return the Words that begin With a capital Letter');
 
