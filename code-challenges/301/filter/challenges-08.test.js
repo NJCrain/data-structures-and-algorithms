09eb9ea73ea677fff5be4007332ebbf3489e7ebd
Nick Crain <nicholascrain@gmail.com>
2018-10-28 19:25:14 -0700

    completed challenge 5

--- code-challenges/301/filter/challenges-08.test.js
+++ code-challenges/301/filter/challenges-08.test.js
@@ -100,8 +100,12 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 ------------------------------------------------------------------------------------------------ */
 
 const getStatName = (arr, minBaseStat) => {
-  // Solution code here...
-};
+    return arr.filter(val => {
+      return val.baseStat > minBaseStat
+    }).map(val => {
+      return val.stat.name;
+    })
+  };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
@@ -236,7 +240,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return the name of the stats that exceed that maximum', () => {
     expect(getStatName(snorlaxData.stats, 50)).toStrictEqual([ 'special-defense', 'special-attack' ]);
     expect(getStatName(snorlaxData.stats, 50).length).toStrictEqual(2);
