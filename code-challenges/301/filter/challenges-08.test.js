d59458d81e7cd543cfd91485a0356702243392cd
Nick Crain <nicholascrain@gmail.com>
2018-10-28 19:29:52 -0700

    completed challenge 7

--- code-challenges/301/filter/challenges-08.test.js
+++ code-challenges/301/filter/challenges-08.test.js
@@ -171,7 +171,16 @@ For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 
 ------------------------------------------------------------------------------------------------ */
 
 const evenOddNumericValues = (arr) => {
-  // Solution code here...
+    return arr.filter(val => {
+        return typeof val === 'number';
+    }).map(val => {
+        if (val % 2) {
+            return 'odd';
+        }
+        else {
+            return 'even';
+        }
+    })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -270,7 +279,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should remove non-integers and return "even" or "odd', () => {
     expect(evenOddNumericValues(['Gregor', 2, 4, 1])).toStrictEqual(['even', 'even', 'odd']);
     expect(evenOddNumericValues(['Gregor', 2, 4, 1]).length).toStrictEqual(3);
