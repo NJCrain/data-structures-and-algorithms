cc2e2f6c5fa25051e6329a93deee31f94fc1585a
Nick Crain <nicholascrain@gmail.com>
2018-10-29 21:27:54 -0700

    completed challenge 3

--- code-challenges/301/regex-p2/challenges-11.test.js
+++ code-challenges/301/regex-p2/challenges-11.test.js
@@ -59,7 +59,8 @@ Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePhoneNumber = (phoneNumber) => {
-  // Solution code here...
+  let regex = /^((\(\d{3})\)|(\d{3}))\-?\s?\d{3}\-?\s?\d{4}$/;
+  return regex.test(phoneNumber);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -128,7 +129,7 @@ describe('Testing challenge 2', () => {
   })
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should match the acceptable phone number formats', () => {
     expect(validatePhoneNumber('(555) 555-5555')).toBeTruthy();
     expect(validatePhoneNumber('555 555-5555')).toBeTruthy();
