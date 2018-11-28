c40d5a8c2880d5e4fbff33a0820c8d1a53ae3362
Nick Crain <nicholascrain@gmail.com>
2018-11-28 09:16:47 -0800

    test file and 1st solution attempt

---
+++ 401/src/test/java/ArrayShiftTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayShiftTest {
+
+    @Test
+    public void insertShiftArray() {
+        int[] numbers = {1, 2, 3, 4};
+        int[] numbersShifted = ArrayShift.insertShiftArray(numbers, 5);
+        //should
+    }
+}
