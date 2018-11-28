b8bfa31cc39d206666b384700327e60301e2f8ca
Nick Crain <nicholascrain@gmail.com>
2018-11-28 09:40:48 -0800

    fixed ordering of 1st assertion

--- 401/src/test/java/ArrayShiftTest.java
+++ 401/src/test/java/ArrayShiftTest.java
@@ -13,7 +13,7 @@ public class ArrayShiftTest {
         //some value before the middle should be at the same index in both arrays
         //some value after the middle should be at an index 1 greater
         assertTrue("The length should be 1 greater than the original array", numbersShifted.length == numbers.length + 1);
-        assertEquals(numbersShifted[numbers.length/2], 5);
+        assertEquals(5, numbersShifted[numbers.length/2]);
         assertEquals(numbers[0], numbersShifted[0]);
         assertEquals(numbers[3], numbersShifted[4]);
     }
