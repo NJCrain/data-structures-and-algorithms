c58664ba6e7350388114c6b94e79d25e71cf0d39
Nick Crain <nicholascrain@gmail.com>
2018-11-28 19:51:58 -0800

    fixed infite looping case, passing 2 tests

--- 401/src/main/java/BinarySearch.java
+++ 401/src/main/java/BinarySearch.java
@@ -1,12 +1,17 @@
 public class BinarySearch {
     public static int binarySearch(int[] arr, int value) {
         int middle = arr.length/2;
-        while (middle >= 0 || middle <  arr.length) {
+        while (middle >= 0) {
             if (arr[middle] == value) {
                 return middle;
             }
             else if (arr[middle] < value) {
-                middle = (middle + arr.length) / 2;
+                if(middle == arr.length -1) {
+                    break;
+                }
+                else {
+                    middle = (middle + arr.length ) / 2;
+                }
             }
             else {
                 middle = middle / 2;
--- 401/src/test/java/BinarySearchTest.java
+++ 401/src/test/java/BinarySearchTest.java
@@ -10,4 +10,9 @@ public class BinarySearchTest {
         //should return the index of the value searched
         assertEquals("The return value should be 4", 4, BinarySearch.binarySearch(numbers, 7));
     }
+
+    @Test public void testBinarySearchNotFound() {
+        int[] numbers = {3, 4, 5, 6, 7};
+        assertEquals("Should return a value of -1", -1, BinarySearch.binarySearch(numbers, 10));
+    }
 }
