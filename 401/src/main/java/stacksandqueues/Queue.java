e606a6365933b04a2f9f7a016db0a797ec790c28
Nick Crain <nicholascrain@gmail.com>
2018-12-10 11:10:54 -0800

    Documentation updates

--- 401/src/main/java/stacksandqueues/Queue.java
+++ 401/src/main/java/stacksandqueues/Queue.java
@@ -9,6 +9,7 @@ public class Queue {
         this.rear = null;
     }
 
+    //Takes in a value and adds a new Node to the end of the queue, with that value
     public void enqueue(int value) {
         if (rear == null) {
             rear = new Node(value, null);
@@ -19,6 +20,7 @@ public class Queue {
         }
     }
 
+    //Removes the front node from the queue and returns its value. Will give a message and return 0 if the queue is empty
     public int dequeue() {
         if (front != null) {
             Node temp = front;
@@ -29,6 +31,7 @@ public class Queue {
         return 0;
     }
 
+    //Returns the front Node of the queue
     public Node peek() {
         return front;
     }
--- 401/src/main/java/stacksandqueues/Stack.java
+++ 401/src/main/java/stacksandqueues/Stack.java
@@ -7,10 +7,12 @@ public class Stack {
         this.top = null;
     }
 
+    //takes in a value and adds a new Node to the top of the stack with that value
     public void push(int value) {
         this.top = new Node(value, top);
     }
 
+    //Will remove the top Node from the stack and return its value. Gives a message and returns 0 for an empty stack
     public int pop() {
         if (top != null) {
             Node temp = top;
@@ -21,6 +23,7 @@ public class Stack {
         return 0;
     }
 
+    //returns the top node of the stack
     public Node peek() {
         return top;
     }
