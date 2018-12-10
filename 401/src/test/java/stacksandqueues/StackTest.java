abe5a6712e3d948ab2f0a61397eaa20c4cf0b01f
Nick Crain <nicholascrain@gmail.com>
2018-12-10 09:42:10 -0800

    testing for Stack

---
+++ 401/src/test/java/stacksandqueues/StackTest.java
@@ -0,0 +1,52 @@
+package stacksandqueues;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+    Stack test = new Stack();
+
+    @Test
+    public void testConstructor() {
+        assertNotNull("Should give back an object", test);
+        assertNull("The top should be null by default", test.top);
+    }
+
+    @Test
+    public void testPush() {
+        test.push(5);
+
+        assertEquals("The top should now point to the new node", 5, test.top.value);
+        assertNull("The new Node's next should be null", test.top.next);
+
+        test.push(4);
+
+        assertEquals("top should now point to the newer node", 4, test.top.value);
+        assertEquals("The next node should be the one we added previously", 5, test.top.next.value);
+    }
+
+    @Test
+    public void testPop() {
+        assertEquals("Popping an empty list should give back a 0", 0, test.pop());
+
+        test.push(5);
+        test.push(4);
+
+        assertEquals("The first pop should give back the last value added", 4, test.pop());
+        assertEquals("Calling pop again should return the value added before 4", 5, test.pop());
+    }
+
+    @Test
+    public void testPeek() {
+        assertNull("Peeking an empty list should give null back", test.peek());
+
+        test.push(4);
+
+        assertEquals("Peeking should now give us the Node we just pushed", 4, test.peek().value);
+
+        test.push(5);
+
+        assertEquals("Adding another Node should still be fine", 5, test.peek().value);
+    }
+}
