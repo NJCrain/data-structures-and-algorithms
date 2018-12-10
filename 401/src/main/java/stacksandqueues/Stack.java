84e353894005e3e4dc6feda3af09c38a6465b48d
Nick Crain <nicholascrain@gmail.com>
2018-12-10 09:27:00 -0800

    stack methods

--- 401/src/main/java/stacksandqueues/Stack.java
+++ 401/src/main/java/stacksandqueues/Stack.java
@@ -6,4 +6,22 @@ public class Stack {
     public Stack() {
         this.top = null;
     }
+
+    public void push(int value) {
+        this.top = new Node(value, top);
+    }
+
+    public int pop() {
+        if (top != null) {
+            Node temp = top;
+            top = top.next;
+            return temp.value;
+        }
+        System.out.println("Can not pop, the stack is currently empty");
+        return 0;
+    }
+
+    public Node peek() {
+        return top;
+    }
 }
