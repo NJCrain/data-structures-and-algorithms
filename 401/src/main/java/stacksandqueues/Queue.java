103f16810b314c79c009373d751898e994a7b38a
Nick Crain <nicholascrain@gmail.com>
2018-12-10 10:23:56 -0800

    Queue methods and tests

--- 401/src/main/java/stacksandqueues/Queue.java
+++ 401/src/main/java/stacksandqueues/Queue.java
@@ -2,8 +2,34 @@ package stacksandqueues;
 
 public class Queue {
     public Node front;
+    public Node rear;
 
     public Queue() {
         this.front = null;
+        this.rear = null;
+    }
+
+    public void enqueue(int value) {
+        if (rear == null) {
+            rear = new Node(value, null);
+            front = rear;
+        } else {
+            rear.next = new Node(value, null);
+            rear = rear.next;
+        }
+    }
+
+    public int dequeue() {
+        if (front != null) {
+            Node temp = front;
+            this.front = front.next;
+            return temp.value;
+        }
+        System.out.println("There is currently nothing in the queue");
+        return 0;
+    }
+
+    public Node peek() {
+        return front;
     }
 }
