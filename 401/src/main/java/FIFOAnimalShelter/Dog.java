5c011261de416c226b2baa061140172f9ec3aa0e
Nick Crain <nicholascrain@gmail.com>
2018-12-11 18:52:01 -0800

    initial class structuring and solution

--- 401/src/main/java/FIFOAnimalShelter/Dog.java
+++ 401/src/main/java/FIFOAnimalShelter/Dog.java
@@ -1,4 +1,12 @@
 package FIFOAnimalShelter;
 
 public class Dog extends Animal {
+    public boolean barks;
+
+    public Dog(String name, int age, String color, boolean barks) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.barks = barks;
+    }
 }
--- 401/src/main/java/FIFOAnimalShelter/FIFOAnimalShelter.java
+++ 401/src/main/java/FIFOAnimalShelter/FIFOAnimalShelter.java
@@ -1,4 +1,32 @@
 package FIFOAnimalShelter;
 
+import stacksandqueues.Queue;
+
 public class FIFOAnimalShelter {
+
+    public Queue<Cat> catQueue;
+    public  Queue<Dog> dogQueue;
+
+    public FIFOAnimalShelter() {
+        this.catQueue = new Queue<>();
+        this.dogQueue = new Queue<>();
+    }
+
+    public void enqueue(Animal toAdd) {
+        if (toAdd instanceof Cat) {
+            catQueue.enqueue((Cat) toAdd);
+        } else {
+            dogQueue.enqueue((Dog) toAdd);
+        }
+    }
+
+    public Animal dequeue(String pref) {
+        if(pref.toLowerCase() == "cat") {
+            return catQueue.dequeue();
+        } else if (pref.toLowerCase() == "dog") {
+            return dogQueue.dequeue();
+        } else {
+            return null;
+        }
+    }
 }
