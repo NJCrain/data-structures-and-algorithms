7684e64fd170a3106585f2c99acecb114889972d
Nick Crain <nicholascrain@gmail.com>
2018-12-12 18:42:20 -0800

    initial solution

--- 401/src/main/java/multibracketvalidation/MultiBracketValidation.java
+++ 401/src/main/java/multibracketvalidation/MultiBracketValidation.java
@@ -1,4 +1,35 @@
 package multibracketvalidation;
 
+import stacksandqueues.Stack;
+
 public class MultiBracketValidation {
+    public boolean multiBracketValidation(String s) {
+        Stack<Character> brackets = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                brackets.push(s.charAt(i));
+            }
+            if (s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}') {
+                char toExpect = ' ';
+                switch (brackets.pop()) {
+                    case '(': toExpect = ')';
+                              break;
+                    case '[': toExpect = ']';
+                              break;
+                    case '{': toExpect = '}';
+                              break;
+                }
+
+                if (toExpect != s.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+
+        if (brackets.pop() != null) {
+            return false;
+        }
+        return true;
+    }
 }
