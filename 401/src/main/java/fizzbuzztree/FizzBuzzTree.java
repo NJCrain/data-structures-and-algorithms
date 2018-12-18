43dbecf1692a1f849b0d87b9b36d515139f0a96e
Nick Crain <nicholascrain@gmail.com>
2018-12-17 17:53:45 -0800

    initial solution

---
+++ 401/src/main/java/fizzbuzztree/FizzBuzzTree.java
@@ -0,0 +1,33 @@
+package fizzbuzztree;
+
+import tree.BinaryTree;
+import tree.Node;
+
+public class FizzBuzzTree {
+    public static BinaryTree<Object> fizzBuzzTree(BinaryTree<Object> t) {
+        fizzBuzz(t.root);
+        return t;
+    }
+
+    private static void fizzBuzz(Node root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            fizzBuzz(root.left);
+        }
+        if ((int) root.value % 3 == 0) {
+            if ((int) root.value % 5 == 0) {
+                root.value = "fizzbuzz";
+            } else {
+                root.value = "fizz";
+            }
+        }
+        if ((int) root.value % 5 == 0) {
+            root.value = "buzz";
+        }
+        if (root.right != null) {
+            fizzBuzz(root.right);
+        }
+    }
+}
