17bcb281589ac46d737a6f9dbaf4a0f5b0050281
Nick Crain <nicholascrain@gmail.com>
2019-01-07 09:58:24 -0800

    addEdge method

--- 401/src/main/java/graph/Graph.java
+++ 401/src/main/java/graph/Graph.java
@@ -2,9 +2,10 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Graph<T> {
-    List<Node<T>> nodes;
+    private List<Node<T>> nodes;
 
     public Graph() {
         this.nodes = new ArrayList<>();
@@ -22,5 +23,23 @@ public class Graph<T> {
         Node<T> n = new Node<>(value);
         this.nodes.add(n);
         return n;
+    }
+
+    public void addEdge(Node n1, Node n2) throws NoSuchElementException {
+        if (this.nodes.contains(n1) && this.nodes.contains(n2)) {
+            n1.neighbors.add(new Edge(n2));
+            n2.neighbors.add(new Edge(n1));
+        } else {
+            throw new NoSuchElementException("Could not find one of the provided Nodes in the graph");
+        }
+    }
+
+    public void addEdge(Node n1, Node n2, int weight) {
+        if (this.nodes.contains(n1) && this.nodes.contains(n2)) {
+            n1.neighbors.add(new Edge(n2, weight));
+            n2.neighbors.add(new Edge(n1, weight));
+        } else {
+            throw new NoSuchElementException("Could not find one of the provided Nodes in the graph");
+        }
     }
 }
