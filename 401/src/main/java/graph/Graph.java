68397dfd70042bcefadcdc6a82a3cf7668dfc0c9
Nick Crain <nicholascrain@gmail.com>
2019-01-07 09:50:25 -0800

    addNode method

--- 401/src/main/java/graph/Graph.java
+++ 401/src/main/java/graph/Graph.java
@@ -3,18 +3,24 @@ package graph;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Graph {
-    List<Node> nodes;
+public class Graph<T> {
+    List<Node<T>> nodes;
 
     public Graph() {
         this.nodes = new ArrayList<>();
     }
 
-    public List<Node> getNodes() {
+    public List<Node<T>> getNodes() {
         return nodes;
     }
 
     public int size() {
         return nodes.size();
+    }
+
+    public Node addNode(T value) {
+        Node<T> n = new Node<>(value);
+        this.nodes.add(n);
+        return n;
     }
 }
