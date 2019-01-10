97b8f97c7013c574bc5eb70b2e0e92d56ca8c92d
Nick Crain <nicholascrain@gmail.com>
2019-01-09 17:56:51 -0800

    initial solution code

---
+++ 401/src/main/java/depthfirst/DepthFirst.java
@@ -0,0 +1,36 @@
+package depthfirst;
+
+import graph.Edge;
+import graph.Graph;
+import graph.Node;
+import stacksandqueues.Stack;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DepthFirst {
+    public static List<Node> depthFirst(Graph g) {
+        Node current =  (Node) g.getNodes().get(0);
+        List<Node> ans = new ArrayList<>();
+        Stack<Node> s = new Stack<>();
+        Set<Node> seen = new HashSet<>();
+
+        s.push(current);
+        seen.add(current);
+
+        while (s.peek() != null) {
+            current = s.pop();
+            ans.add(current);
+            for (Edge e : (List<Edge>) current.neighbors) {
+                if (!seen.contains(e.neighbor)) {
+                    seen.add(e.neighbor);
+                    s.push(e.neighbor);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
