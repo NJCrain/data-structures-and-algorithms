2be84dcfb33f215b51375ba1761389a1e223ae5e
Nick Crain <nicholascrain@gmail.com>
2019-01-14 12:18:03 -0800

    comments for methods

--- 401/src/main/java/hashtable/Hashtable.java
+++ 401/src/main/java/hashtable/Hashtable.java
@@ -6,10 +6,12 @@ import java.util.NoSuchElementException;
 public class Hashtable<T1, T2> {
     private LinkedList<String>[] buckets;
 
+    //Constructor requires a size to create the table
     public Hashtable (int size) {
         this.buckets = new LinkedList[size];
     }
 
+    //private method to generate hash values for the table
     private int getHash(T1 key) {
        int hashValue = 0;
        String keyToHash = key.toString();
@@ -20,6 +22,7 @@ public class Hashtable<T1, T2> {
        return hashValue;
     }
 
+    //takes in a key and a value, gets the keys hash, stores the key + value at the hashed index
     public void add(T1 key, T2 value) {
         int idx = getHash(key);
         if (buckets[idx] == null) {
@@ -28,6 +31,7 @@ public class Hashtable<T1, T2> {
         buckets[idx].add(key + ": " + value);
     }
 
+    //Takes in a key, returns true if the given key is already in the table, false if not.
     public boolean contains(T1 key) {
         int idx = getHash(key);
         if (buckets[idx] == null) {
@@ -43,20 +47,24 @@ public class Hashtable<T1, T2> {
         }
     }
 
+    //Takes in a key and returns the value associated with that key. Throws a NoSuchElementException if no values are found for that key
+    //TODO: try to figure out why this can return things that aren't of type T2
     public T2 find(T1 key) throws NoSuchElementException {
         int idx= getHash(key);
         if (buckets[idx].peek() == null) {
-            throw new NoSuchElementException("No such value found for the key" + key);
+            throw new NoSuchElementException("No such value found for the key " + key);
         }
         for (int i = 0; i < buckets[idx].size(); i++) {
             String current = buckets[idx].get(i);
             if (current.contains(key.toString())) {
-                return (T2) current.split(": ")[1];
+                T2 value = (T2) (current.split(": ")[1]);
+                return value;
             }
         }
         return (T2) buckets[idx].peek().split(": ")[1];
     }
 
+    //Returns the size of the Hashtable
     public int size() {
         return buckets.length;
     }
