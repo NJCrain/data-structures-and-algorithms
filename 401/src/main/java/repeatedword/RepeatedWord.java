be6887f4258ec8298b06e684c0bae06ff0a36235
Nick Crain <nicholascrain@gmail.com>
2019-01-14 17:49:17 -0800

    initial solution code

--- 401/src/main/java/repeatedword/RepeatedWord.java
+++ 401/src/main/java/repeatedword/RepeatedWord.java
@@ -1,8 +1,24 @@
 package repeatedword;
 
-public class RepeatedWord {
-    public static String firstRepeat(String s) {
+import java.util.HashSet;
+import java.util.Set;
 
-        return "No repeated words found";
+public class RepeatedWord {
+    public static String firstRepeat(String s) throws Exception {
+        Set<String> words = new HashSet<>();
+        int lastChar = 0;
+
+        for (int i = 0; i < s.length(); i++)
+            if (s.charAt(i) == ' ') {
+                String word = s.substring(lastChar, i - 1);
+                lastChar = i + 1;
+                if (words.contains(word.toLowerCase())) {
+                    return word;
+                } else {
+                    words.add(word.toLowerCase());
+                }
+            }
+
+        throw new Exception("No repeating words found");
     }
 }
