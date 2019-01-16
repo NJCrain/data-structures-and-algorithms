d472271a20665b98ae5a8dbca01c701aee410fef
Nick Crain <nicholascrain@gmail.com>
2019-01-15 17:58:39 -0800

    second test case

--- 401/src/test/java/treeintersection/TreeIntersectionTest.java
+++ 401/src/test/java/treeintersection/TreeIntersectionTest.java
@@ -14,11 +14,14 @@ import static treeintersection.TreeIntersection.findCommonValues;
 class TreeIntersectionTest {
     BinaryTree<Integer> t1 = new BinaryTree<>();
     BinaryTree<Integer> t2 = new BinaryTree<>();
+    BinaryTree<Integer> t3 = new BinaryTree<>();
 
     @BeforeEach
     void setup() {
         t1.root = new Node<Integer>(1, new Node<>(2, new Node<>(4), new Node<>(5)), new Node<>(3, new Node<>(6), null));
         t2.root = new Node<Integer>(1, new Node<>(3, new Node<>(7), null), new Node<>(5, null, new Node<>(9)));
+        t3.root = new Node<Integer>(2, new Node<>(4, new Node<>(8), null), new Node<>(6, null, new Node<>(10)));
+
     }
 
     @Test
@@ -29,5 +32,15 @@ class TreeIntersectionTest {
         assertEquals(1, iterator.next());
         assertEquals(3, iterator.next());
         assertEquals(5, iterator.next());
+    }
+
+    @Test
+    void testTreeIntersection_NoIntersections() {
+        Set results = findCommonValues(t2, t3);
+        assertEquals(0, results.size(), "There should be 0 common values in the set");
+//        Iterator iterator = results.iterator();
+//        assertEquals(1, iterator.next());
+//        assertEquals(3, iterator.next());
+//        assertEquals(5, iterator.next());
     }
 }
