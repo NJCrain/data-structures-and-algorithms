1a7533e329f5537a68c0554590d7b952f602b438
Nick Crain <nicholascrain@gmail.com>
2019-01-16 18:45:04 -0800

    Real tests

--- 401/src/test/java/leftjoin/LeftJoinTest.java
+++ 401/src/test/java/leftjoin/LeftJoinTest.java
@@ -4,8 +4,6 @@ import hashtable.Hashtable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static leftjoin.LeftJoin.leftJoin;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,8 +29,51 @@ class LeftJoinTest {
     void testLeftJoin() {
 
         String[][] results = leftJoin(table1, table2);
-        for (String[] set : results) {
-            System.out.println(Arrays.toString(set));
+        String[][] expected = new String[][]{{"fond", "enamored", "averse"}, {"wrath", "angered", "delight"}, {"outfit", "garb", null}};
+        assertEquals(table1.getKeys().size(), results.length);
+        for (int i = 0; i < expected.length; i++) {
+            for (int j = 0; j < expected[i].length; j++) {
+                assertEquals(expected[i][j], results[i][j]);
+            }
         }
+    }
+
+    @Test
+    void testLeftJoin_NoMatches() {
+        Hashtable<String, String> table3 = new Hashtable<>(8);
+        table3.add("pond", "ocean");
+        table3.add("stuff", "things");
+
+        String[][] results = leftJoin(table1, table3);
+        String[][] expected = new String[][]{{"fond", "enamored", null}, {"wrath", "angered", null}, {"outfit", "garb", null}};
+        assertEquals(table1.getKeys().size(), results.length);
+        for (int i = 0; i < expected.length; i++) {
+            for (int j = 0; j < expected[i].length; j++) {
+                assertEquals(expected[i][j], results[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void testLeftJoin_EmptyTable() {
+        Hashtable<String, String> table3 = new Hashtable<>(8);
+
+        String[][] results = leftJoin(table1, table3);
+        String[][] expected = new String[][]{{"fond", "enamored", null}, {"wrath", "angered", null}, {"outfit", "garb", null}};
+        assertEquals(table1.getKeys().size(), results.length);
+        for (int i = 0; i < expected.length; i++) {
+            for (int j = 0; j < expected[i].length; j++) {
+                assertEquals(expected[i][j], results[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void testLeftJoin_BothTablesEmpty() {
+        Hashtable<String, String> table3 = new Hashtable<>(8);
+        Hashtable<String, String> table4 = new Hashtable<>(8);
+
+        String[][] results = leftJoin(table4, table3);
+        assertEquals(0, results.length);
     }
 }
