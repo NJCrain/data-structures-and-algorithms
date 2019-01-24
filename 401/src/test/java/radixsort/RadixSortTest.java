5844ca724820e70f66c099ab26cf1736bcf7709f
Nick Crain <nicholascrain@gmail.com>
2019-01-24 09:36:28 -0800

    README, documentation, and tests, oh my!

--- 401/src/test/java/radixsort/RadixSortTest.java
+++ 401/src/test/java/radixsort/RadixSortTest.java
@@ -2,7 +2,7 @@ package radixsort;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static radixsort.RadixSort.radixSort;
@@ -17,6 +17,36 @@ class RadixSortTest {
         radixSort(unsorted);
 
         assertArrayEquals(sorted, unsorted);
+    }
+
+    @Test
+    void testRadixSort_OddLength() {
+        int[] unsorted = {121, 324, 431, 315, 134};
+        int[] sorted = {121, 134, 315, 324, 431};
+
+        radixSort(unsorted);
+
+        assertArrayEquals(sorted, unsorted);
+    }
+
+    @Test
+    void testRadixSort_BigArray() {
+        int[] unsorted = new Random().ints(1000, 1000, 9999).toArray();
+        radixSort(unsorted);
+
+        for (int i = 1; i < unsorted.length - 1; i++) {
+            assertTrue(unsorted[i] >= unsorted[i - 1] && unsorted[i] <= unsorted[i + 1], "All values should be greater than the one before it, and less than the one after it");
+        }
+    }
+
+    @Test
+    void testRadixSort_BigArrayOddLength() {
+        int[] unsorted = new Random().ints(1001, 1000, 9999).toArray();
+        radixSort(unsorted);
+
+        for (int i = 1; i < unsorted.length - 1; i++) {
+            assertTrue(unsorted[i] >= unsorted[i - 1] && unsorted[i] <= unsorted[i + 1], "All values should be greater than the one before it, and less than the one after it");
+        }
     }
 
 }
