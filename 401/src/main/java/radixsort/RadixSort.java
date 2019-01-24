099e57019da5ffdba6b1674229bc778282d4fc4f
Nick Crain <nicholascrain@gmail.com>
2019-01-23 18:46:19 -0800

    initial solution code

--- 401/src/main/java/radixsort/RadixSort.java
+++ 401/src/main/java/radixsort/RadixSort.java
@@ -1,4 +1,24 @@
 package radixsort;
 
+import java.util.LinkedList;
+
+import static java.lang.Integer.valueOf;
+
 public class RadixSort {
+    public static void radixSort(Integer[] nums) {
+        int digits = nums[0].toString().length();
+        for (int i = 0; i < digits; i++) {
+            LinkedList[] llArr = new LinkedList[10];
+            for (int j = 0; j < nums.length; j++) {
+                int val = valueOf(nums[j].toString().charAt(i));
+                llArr[val].add(nums[j]);
+            }
+            int idx = 0;
+            for (LinkedList list : llArr) {
+                while (list.peek() != null);
+                nums[i] = (Integer) list.remove();
+                idx++;
+            }
+        }
+    }
 }
